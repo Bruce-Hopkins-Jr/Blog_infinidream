1f6c6762d6e5751a782ae91257700007c2d2da80
Bruce-Hopkins-Jr <behopkinsjr@gmail.com>
2021-09-09 12:08:42 +0200

    changed session secret

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({secret: "fb!ywefjh3v908#"}));
+app.use(session({secret: process.env.ORIGIN}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
